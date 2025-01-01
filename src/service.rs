16eb7c716aef4f510eb52c6342ffda55205ce01b
Max Countryman <maxc@me.com>
2025-01-01 08:49:43 -0800

    axum-core v0.5

--- src/service.rs
+++ src/service.rs
@@ -126,7 +126,7 @@ impl<'a> SessionConfig<'a> {
     }
 }
 
-impl<'a> Default for SessionConfig<'a> {
+impl Default for SessionConfig<'_> {
     fn default() -> Self {
         Self {
             name: "id".into(), /* See: https://cheatsheetseries.owasp.org/cheatsheets/Session_Management_Cheat_Sheet.html#session-id-name-fingerprinting */
@@ -548,9 +548,8 @@ mod tests {
     use tower::{ServiceBuilder, ServiceExt};
     use tower_sessions_memory_store::MemoryStore;
 
-    use crate::session::{Id, Record};
-
     use super::*;
+    use crate::session::{Id, Record};
 
     async fn handler(req: Request<Body>) -> anyhow::Result<Response<Body>> {
         let session = req
@@ -792,7 +791,7 @@ mod tests {
         let sid1 = get_session_id(&res1);
         let rec1 = get_record(&session_store, &sid1).await;
         let req2 = Request::builder()
-            .header(http::header::COOKIE, &format!("id={}", sid1))
+            .header(http::header::COOKIE, format!("id={}", sid1))
             .body(Body::empty())?;
         let res2 = svc.call(req2).await?;
         let sid2 = get_session_id(&res2);
@@ -821,7 +820,7 @@ mod tests {
         let sid1 = get_session_id(&res1);
         let rec1 = get_record(&session_store, &sid1).await;
         let req2 = Request::builder()
-            .header(http::header::COOKIE, &format!("id={}", sid1))
+            .header(http::header::COOKIE, format!("id={}", sid1))
             .body(Body::empty())?;
         let res2 = svc.call(req2).await?;
         let sid2 = get_session_id(&res2);
@@ -851,7 +850,7 @@ mod tests {
         let sid1 = get_session_id(&res1);
         let rec1 = get_record(&session_store, &sid1).await;
         let req2 = Request::builder()
-            .header(http::header::COOKIE, &format!("id={}", sid1))
+            .header(http::header::COOKIE, format!("id={}", sid1))
             .body(Body::empty())?;
         let res2 = svc.call(req2).await?;
         let sid2 = get_session_id(&res2);
